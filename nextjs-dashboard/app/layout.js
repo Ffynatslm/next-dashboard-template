471f576cb8ca06f4bf2a32680e519a072f4b4347
ffynatslm <105841104522@student.unismuh.ac.id>
2024-08-14 18:56:30 +0700

    chapter 3 selesai

--- nextjs-dashboard/app/layout.js
+++ nextjs-dashboard/app/layout.js
@@ -1,9 +1,8 @@
-import "@/app/ui/global.css";
-
+import { lusitana } from "./ui/fonts";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
