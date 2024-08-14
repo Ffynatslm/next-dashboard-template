136fcf7c7d036a09b09293d1097fb72bd73c2f96
ffynatslm <105841104522@student.unismuh.ac.id>
2024-08-14 16:24:49 +0700

    selesai chapter 2

--- nextjs-dashboard/app/page.js
+++ nextjs-dashboard/app/page.js
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
 export default function Page() {
+  
   return (
     <main className="flex flex-col min-h-screen p-6">
       <div className="flex items-end h-20 p-4 bg-blue-500 rounded-lg shrink-0 md:h-52">
